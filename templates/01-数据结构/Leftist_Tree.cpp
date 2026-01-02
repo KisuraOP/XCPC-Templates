7581c3aea57eedf9c2f5518f2b73eb912b5a250e
KisuraOP <2389552731@qq.com>
2026-01-02 15:29:20 +0800

    v3.5.1 Upd Leftist_Tree

--- templates/01-数据结构/Leftist_Tree.cpp
+++ templates/01-数据结构/Leftist_Tree.cpp
@@ -5,9 +5,9 @@ leftistTree G(n, m);
 其中 n 表示集合数量, m 表示往集合里插入的元素数量总和 (即插入操作的次数上限)。
 集合编号为 1 ~ n。
 
-以下每一种操作的时间复杂度均为 O(log m)。
+以下每一种操作的时间复杂度均不高于 O(log m)。
 
-int id = add(x, y);
+int id = insert(x, y);
 往编号为 x 的集合里插入一个值为 y 的元素, 返回该元素的编号。
 
 int id = get(x);
@@ -28,12 +28,15 @@ dkey(x, y);
 把 **编号** 为 x 的元素的值更改为 y。
 需要保证 y 严格小于更改前 **编号** 为 x 的元素的值。
 
+add(x, y);
+把编号为 x 的集合里的所有元素的值加上 y。
+
 rt[x] 表示编号为 x 的集合的堆顶元素的编号。
 */
 struct leftistTree {
 	struct Node {
-		int l, r, d, fa, v;
-		Node() : l(0), r(0), d(0), fa(0), v(0) {}
+		int l, r, d, fa, v, tag;
+		Node() : l(0), r(0), d(0), fa(0), v(0), tag(0) {}
 	};
 	vector<Node> a;
 	vector<int> rt;
@@ -44,27 +47,34 @@ struct leftistTree {
 	#define d(x) a[x].d
 	#define fa(x) a[x].fa
 	#define v(x) a[x].v
+	#define tag(x) a[x].tag
 
 	int _find(int x) {
 		return p[x] == x ? x : p[x] = _find(p[x]);
 	}
 	void _union(int x, int y) {
-		int t = rt[x];
-		rt[x] = _merge(t, y);
-		fa(rt[x]) = 0;
-		if (t) {
-			p[_find(t)] = _find(rt[x]);
+		if (!rt[x]) {
+			rt[x] = y;
+			bel[_find(rt[x])] = x;
+		} else {
+			int z = rt[x];
+			rt[x] = _merge(rt[x], y);
+			fa(rt[x]) = 0;
+			p[_find(z)] = _find(rt[x]);
+			p[_find(y)] = _find(rt[x]);
+			bel[_find(rt[x])] = x;
 		}
-		p[_find(y)] = _find(rt[x]);
-		bel[_find(rt[x])] = x;
 	}
 	int _merge(int x, int y) {
 		if (!x || !y) {
 			return x | y;
 		}
-		if (v(x) > v(y) || (v(x) == v(y) && x > y)) {
+		int vx = v(x) + tag(x);
+		int vy = v(y) + tag(y);
+		if (vx > vy || (vx == vy && x > y)) {
 			swap(x, y);
 		}
+		tag(y) -= tag(x);
 		r(x) = _merge(r(x), y);
 		fa(r(x)) = x;
 		if (d(l(x)) < d(r(x))) {
@@ -91,10 +101,9 @@ struct leftistTree {
 		a.push_back(Node());
 		d(0) = -1;
 	}
-	int add(int x, int y) {
+	int insert(int x, int y) {
 		a.push_back(Node());
 		int z = a.size() - 1;
-		d(z) = l(z) = r(z) = fa(z) = 0;
 		v(z) = y;
 		_union(x, z);
 		return z;
@@ -109,44 +118,50 @@ struct leftistTree {
 		if (x == y || !rt[y]) {
 			return;
 		}
-		if (!rt[x]) {
-			rt[x] = rt[y];
-			bel[_find(rt[x])] = x;
-		} else {
-			_union(x, rt[y]);
-		}
+		_union(x, rt[y]);
 		rt[y] = 0;
 	}
 	int top(int x) {
-		return !rt[x] ? -inf : v(rt[x]);
+		return !rt[x] ? -inf : v(rt[x]) + tag(rt[x]);
 	}
 	void erase(int x) {
 		if (x >= a.size() || d(x) == -2) {
 			return;
+		}
+		if (l(x)) {
+			tag(l(x)) += tag(x);
+		}
+		if (r(x)) {
+			tag(r(x)) += tag(x);
 		}
 		int f = fa(x);
 		int q = _merge(l(x), r(x));
 		fa(q) = f;
 		if (f == 0) {
 			rt[get(x)] = q;
+			if (q) {
+				fa(q) = 0;
+				bel[_find(q)] = get(x); 
+			}
 		} else {
 			l(f) == x ? l(f) = q : r(f) = q;
 			_pushup(f);
 		}
-		d(x) = -2;
+		d(x) = -2; 
 	}
 	void dkey(int x, int y) {
 		if (x >= a.size() || d(x) == -2) {
 			return;
 		}
-		v(x) = y;
-		int f = fa(x);
-		if (f == 0 || v(x) >= v(f)) {
-			return;
+		int z = get(x);
+		erase(x);
+		d(x) = l(x) = r(x) = fa(x) = tag(x) = 0;
+		v(x) = y; 
+		_union(z, x);
+	}
+	void add(int x, int y) {
+		if (rt[x]) {
+			tag(rt[x]) += y;
 		}
-		l(f) == x ? l(f) = 0 : r(f) = 0;
-		_pushup(f);
-		fa(x) = 0;
-		_union(get(x), x);
 	}
 };
